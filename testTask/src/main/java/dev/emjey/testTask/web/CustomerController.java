de14fd09e339e0b35e6c317bb9a2f1b073c2050a
emjeydev <emjeydev@gmail.com>
2024-07-02 16:05:31 +0330

    add customer Controller

--- testTask/src/main/java/dev/emjey/testTask/web/CustomerController.java
+++ testTask/src/main/java/dev/emjey/testTask/web/CustomerController.java
@@ -1,4 +1,38 @@
 package dev.emjey.testTask.web;
 
+import dev.emjey.testTask.entity.Customer;
+import dev.emjey.testTask.service.CustomerService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+    CustomerService customerService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable Long id) {
+        return new ResponseEntity<>(customerService.getCustomerById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> saveCustomer(@RequestBody Customer course) {
+        return new ResponseEntity<>(customerService.saveCustomer(course), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Customer>> getCustomers() {
+        return new ResponseEntity<>(customerService.getAllCustomers(), HttpStatus.OK);
+    }
 }
